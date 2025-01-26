8f330f981f2196ab2a4229b97f5a4927382846fa
blcoussama <belcadioussama.eng.com>
2025-01-26 17:27:25 +0100

    Build the Credentials Function from Auth.js V5

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from "@/auth"
+
+const DashboardPage = async() => {
+
+    const session = await auth()
+    console.log(session)
+
+    return (
+        <div>Dashbarp age</div>
+    )
+}
+
+export default DashboardPage
--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,10 @@
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+import { LoginSchema } from "./schemas" 
+import { prisma } from "./prisma/prisma"
+import bcrypt from "bcryptjs"
+
 
 export default {
     providers: [
@@ -9,7 +13,31 @@ export default {
             clientSecret: process.env.AUTH_GOOGLE_SECRET, 
         }),
         Credentials({
+            async authorize(credentials) {
+                const validatedData = LoginSchema.safeParse(credentials);
 
+                if(!validatedData) return null;
+
+                const { email, password } = validatedData.data;
+
+                const user = await prisma.user.findFirst({
+                    where: {
+                        email: email,
+                    }
+                })
+
+                if(!user || !user.password || !user.email) {
+                    return null;
+                }
+
+                const passwordsMatch = await bcrypt.compare(password, user.password)
+
+                if(passwordsMatch) {
+                    return user;
+                }
+
+                return null;
+            }
         })
     ]
 } satisfies NextAuthConfig;
