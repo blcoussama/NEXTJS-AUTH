dbd090b2e7ab84d10a92966beac9dd1c291dab3c
blcoussama <belcadioussama.eng.com>
2025-01-26 13:01:49 +0100

    Setting Up the Sign In with Google Account

--- auth.config.ts
+++ auth.config.ts
@@ -5,7 +5,8 @@ import type { NextAuthConfig } from "next-auth";
 export default {
     providers: [
         Google({
-
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET, 
         }),
         Credentials({
 
