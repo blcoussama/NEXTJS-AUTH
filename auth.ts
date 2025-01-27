2da73707b542323cd60cfaf85bf6883ffae99e55
blcoussama <belcadioussama.eng.com>
2025-01-27 12:40:04 +0100

    Completing The Jwt CallBack Function & Coding the Session CallBack Function

--- auth.ts
+++ auth.ts
@@ -2,6 +2,9 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./prisma/prisma";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user"
+import { getAccountByUserId } from "./data/account"
+
 
 export const { auth, handlers: { GET, POST }, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
@@ -9,8 +12,31 @@ export const { auth, handlers: { GET, POST }, signIn, signOut } = NextAuth({
     ...authConfig,
     callbacks: {
         async jwt({token}) {
-            console.log('jwt', token)
+
+            if(!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub)
+            if(!existingUser) return token;
+
+            const existingAccount = await getAccountByUserId(existingUser.id)
+
+            token.isOauth = !!existingAccount;
+            token.name = existingUser.name
+            token.email = existingUser.email
+            token.image = existingUser.image
+
             return token
-        }
+        },
+        async session({ token, session }) {
+
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.sub,
+                    isOauth: token.isOauth,
+                }    
+            }
+        }   
     }
 })
