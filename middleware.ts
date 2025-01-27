e22274393b2be2b0f8f2ea14fb6e1e9533113187
blcoussama <belcadioussama.eng.com>
2025-01-27 13:20:22 +0100

    Defining teh private routes & Coding the Logic for the middleware Function & Testing it

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,45 @@
 import authConfig from "./auth.config"
 import NextAuth from "next-auth"
- 
-// Use only one of the two middleware options below
-// 1. Use middleware directly
-// export const { auth: middleware } = NextAuth(authConfig)
+import { privateRoutes } from "./routes"
  
 // 2. Wrapped middleware option
 const { auth } = NextAuth(authConfig)
 export default auth(async(req) => {
-  // Your custom middleware logic goes here
   // testing if the midddleware is called
   console.log("middleware called: ", req.nextUrl.pathname)
   // test if we get an object back of the user
   console.log(req.auth)
+
+  // middleware logic goes here
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req
+  const url = "http://localhost:3000";
+
+  // Define the Private Routes
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
+
+  // Define the Auth Routes
+  const isAuthRoute = nextUrl.pathname.includes("/auth")
+
+  // Define the Api Routes
+  const isApiRoute = nextUrl.pathname.includes("/api")
+
+  if(isApiRoute) {
+    return;
+  }
+
+  if(isLoggedIn && isAuthRoute) {
+    return Response.redirect(`${url}/dashboard`);
+  }
+
+  if(isAuthRoute && !isLoggedIn) {
+    return;
+  }
+
+  if(!isLoggedIn && isPrivateRoute) {
+    return Response.redirect(`${url}/auth/login `)
+  }
+
 })
 
 export const config = {
